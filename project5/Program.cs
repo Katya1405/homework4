ab5f51d32ee593115516540658919814da7f8719
Katya <bozhkova-ekaterina@mail.ru>
2022-10-12 16:11:58 +0300

    добавили решение 5 задачи без исп методов

---
+++ project5/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+Console.WriteLine("Задайте размер массива: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int[] array = new int[n];
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100, 100);
+    Console.Write(array[i] + " ");
+    if (i % 2 == 1) sum += array[i];
+}
+Console.WriteLine();
+Console.WriteLine("Сумма чисел массива, стоящих на нечетной позиции= " + sum);
