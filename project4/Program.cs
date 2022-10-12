2f50ad359c2b49098299d58a8bb0874663a2a062
Katya <bozhkova-ekaterina@mail.ru>
2022-10-12 16:00:07 +0300

    добавили задачу без исп методов

---
+++ project4/Program.cs
@@ -0,0 +1,12 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int[] arrayushka = new int[123];
+int count = 0;
+for (int i = 0; i < arrayushka.Length; i++)
+{
+    arrayushka[i] = i + 1;
+    Console.Write(arrayushka[i] + " ");
+    if (arrayushka[i] >= 10 && arrayushka[i] <= 99) count++;
+}
+Console.WriteLine();
+Console.WriteLine("В данном массиве из отрезка [10,99] присуствует " + count + " чисел");
