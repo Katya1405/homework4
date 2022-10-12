24098abb7d54657c06628044decb3b87c429f544
Katya <bozhkova-ekaterina@mail.ru>
2022-10-12 17:15:16 +0300

    добавили задачу 6

---
+++ project6/Program.cs
@@ -0,0 +1,22 @@
+﻿// Найти произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+Console.Write("Задайте размер массива: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int[] array = new int[n];
+int product = 1;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100, 100);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+for (int i = 0; i < array.Length; i++)
+{
+    if (i < array.Length / 2)
+    {
+        product = array[i] * array[n - 1];
+        n--;
+        Console.WriteLine("Произведение пары = " + product);
+    }
+}
