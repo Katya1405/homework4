d1b015850de23591bc28a1077566868fd4887c49
Katya <bozhkova-ekaterina@mail.ru>
2022-10-12 17:44:56 +0300

    добавили 7 задачу

---
+++ project7/Program.cs
@@ -0,0 +1,14 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+
+double[] array = { -17.6, 4.245, 99.42, 8, -9.38 };
+double min = array[0];
+double max = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < min) min = array[i];
+    else if (array[i] > max) max = array[i];
+}
+Console.WriteLine("Максимальный элемент в массиве: " + max);
+Console.WriteLine("Минимальный элемент в массиве: " + min);
+double result = max - min;
+Console.WriteLine("Разница между max и min элементами = " + result);
