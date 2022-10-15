d2e0b6a1402f880441c371c1a7b0ae3867d59c28
Katya <bozhkova-ekaterina@mail.ru>
2022-10-15 13:04:57 +0300

    добавили методы

--- project3/Program.cs
+++ project3/Program.cs
@@ -3,19 +3,35 @@
 // countEven - количество четных чисел
 // count Odd - колво нечетных
 
+void fillArray(int[] arraychik, int min, int max)
+{
+    for (int i = 0; i < arraychik.Length; i++)
+        arraychik[i] = new Random().Next(min, max);
+}
+
+void printArray(int[] arraychik)
+{
+    for (int i = 0; i < arraychik.Length; i++)
+        Console.Write(arraychik[i] + " ");
+}
 
 int n = new Random().Next(1, 15);
 Console.WriteLine("Размерность массива: " + n);
 int[] arraychik = new int[n];
+
+fillArray(arraychik, 100, 1000);
+printArray(arraychik);
+
 int countEven = 0;
 int countOdd = 0;
+
 for (int i = 0; i < arraychik.Length; i++)
 {
-    arraychik[i] = new Random().Next(100, 1000);
-    Console.Write(arraychik[i] + " ");
-    if (arraychik[i] % 2 == 0) countEven++;
+    if (arraychik[i] % 2 == 0)
+        countEven++;
     else countOdd++;
 }
+
 Console.WriteLine();
 Console.WriteLine("Количество четных чисел в данном массиве: " + countEven);
 Console.WriteLine("Количество нечетных чисел в данном массиве: " + countOdd);
