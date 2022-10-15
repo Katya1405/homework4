3734dd991fa8d0a6334188acc7e372859101ed87
Katya <bozhkova-ekaterina@mail.ru>
2022-10-15 13:49:08 +0300

    добавили методы

--- project5/Program.cs
+++ project5/Program.cs
@@ -1,14 +1,36 @@
 ﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
 
+
+void fillArray(int[] array, int min, int max)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(min, max);
+}
+
+void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+}
+
+int SumNumbers(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1)
+            sum += array[i];
+    }
+    return sum;
+}
+
 Console.WriteLine("Задайте размер массива: ");
 int n = int.Parse(Console.ReadLine() ?? "0");
 int[] array = new int[n];
-int sum = 0;
-for (int i = 0; i < array.Length; i++)
-{
-    array[i] = new Random().Next(-100, 100);
-    Console.Write(array[i] + " ");
-    if (i % 2 == 1) sum += array[i];
-}
+
+fillArray(array, -100, 100);
+printArray(array);
+int sum = SumNumbers(array);
+
 Console.WriteLine();
 Console.WriteLine("Сумма чисел массива, стоящих на нечетной позиции= " + sum);
