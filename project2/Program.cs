9e5b3f0db9178477d9a0a82f6ac38490a51e74d4
Katya <bozhkova-ekaterina@mail.ru>
2022-10-15 12:28:42 +0300

    добавили методы

--- project2/Program.cs
+++ project2/Program.cs
@@ -4,14 +4,29 @@
 // sumPos - сумма положительных элементов
 // sumNeg - сумма отрицательных элементов 
 
+void fillArray(int[] myArray, int min, int max)
+{
+    for (int i = 0; i < myArray.Length; i++)
+        myArray[i] = new Random().Next(min, max);
+}
+
+void printArray(int[] myArray)
+{
+    for (int i = 0; i < myArray.Length; i++)
+        Console.Write(myArray[i] + " ");
+}
+
 int[] myArray = new int[12];
 int sumPos = 0;
 int sumNeg = 0;
+
+fillArray(myArray, 0, 10);
+printArray(myArray);
+
 for (int i = 0; i < myArray.Length; i++)
 {
-    myArray[i] = new Random().Next(0, 10);
-    Console.Write(myArray[i] + " ");
-    if (myArray[i] > 0) sumPos += myArray[i];
+    if (myArray[i] > 0)
+        sumPos += myArray[i];
     else sumNeg += myArray[i];
 }
 Console.WriteLine();
