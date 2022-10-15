b12631dde6b9397960c2114c2c7ebb55da4cefd4
Katya <bozhkova-ekaterina@mail.ru>
2022-10-15 12:10:05 +0300

    добавили методы

--- project1/Program.cs
+++ project1/Program.cs
@@ -1,9 +1,20 @@
 ﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
 
+void fillArray(int[] array, int min, int max)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(min, max);
+}
+
+void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+}
+
 int n = 8;
 int[] array = new int[n];
-for (int i = 0; i < array.Length; i++)
-{
-    array[i] = new Random().Next(0, 2);
-    Console.Write(array[i] + " ");
-}
+
+fillArray(array, 0, 2);
+printArray(array);
+
