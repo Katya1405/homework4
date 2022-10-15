a552f6e587108ca66dab681710b0b61102393b0d
Katya <bozhkova-ekaterina@mail.ru>
2022-10-15 13:29:40 +0300

    добавили методы

--- project4/Program.cs
+++ project4/Program.cs
@@ -1,12 +1,34 @@
 ﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
 
-int[] arrayushka = new int[123];
-int count = 0;
-for (int i = 0; i < arrayushka.Length; i++)
+
+void fillArray(int[] arrayushka)
 {
-    arrayushka[i] = i + 1;
-    Console.Write(arrayushka[i] + " ");
-    if (arrayushka[i] >= 10 && arrayushka[i] <= 99) count++;
+    for (int i = 0; i < arrayushka.Length; i++)
+        arrayushka[i] = i + 1;
 }
+
+void printArray(int[] arrayushka)
+{
+    for (int i = 0; i < arrayushka.Length; i++)
+        Console.Write(arrayushka[i] + " ");
+}
+
+int Count(int[] arrayushka)
+{
+    int count = 0;
+    for (int i = 0; i < arrayushka.Length; i++)
+    {
+        if (arrayushka[i] >= 10 && arrayushka[i] <= 99)
+            count++;
+    }
+    return count;
+}
+
+int[] arrayushka = new int[123];
+
+fillArray(arrayushka);
+printArray(arrayushka);
+int count = Count(arrayushka);
+
 Console.WriteLine();
-Console.WriteLine("В данном массиве из отрезка [10,99] присуствует " + count + " чисел");
+Console.WriteLine($"В данном массиве из отрезка [10,99] присуствует {count} чисел");
