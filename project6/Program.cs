9d63d5b02b75c4f88c8aa2bd2e84eb3c72c620a4
Katya <bozhkova-ekaterina@mail.ru>
2022-10-15 22:17:01 +0300

    исправили 6-7 задачу

--- project6/Program.cs
+++ project6/Program.cs
@@ -1,22 +1,34 @@
 ﻿// Найти произведение пар чисел в одномерном массиве. 
 // Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
+
+void fillArray(int[] array, int min, int max)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(min, max);
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+}
+
 Console.Write("Задайте размер массива: ");
 int n = int.Parse(Console.ReadLine() ?? "0");
 int[] array = new int[n];
 int product = 1;
-for (int i = 0; i < array.Length; i++)
-{
-    array[i] = new Random().Next(-100, 100);
-    Console.Write(array[i] + " ");
-}
+
+fillArray(array, -100, 100);
+PrintArray(array);
 Console.WriteLine();
+
 for (int i = 0; i < array.Length; i++)
 {
     if (i < array.Length / 2)
     {
         product = array[i] * array[n - 1];
+        Console.WriteLine($"Пара {array[i]} * {array[n - 1]} = {product}");
         n--;
-        Console.WriteLine("Произведение пары = " + product);
     }
 }
--- project7/Program.cs
+++ project7/Program.cs
@@ -1,14 +1,16 @@
 ﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
 
-double[] array = { -17.6, 4.245, 99.42, 8, -9.38 };
-double min = array[0];
-double max = array[0];
+float[] array = { -17.6f, 4.245f, 99.42f, 8, -9.38f };
+float min = array[0];
+float max = array[0];
 for (int i = 0; i < array.Length; i++)
 {
-    if (array[i] < min) min = array[i];
-    else if (array[i] > max) max = array[i];
+    if (array[i] < min)
+        min = array[i];
+    else if (array[i] > max) 
+        max = array[i];
 }
 Console.WriteLine("Максимальный элемент в массиве: " + max);
 Console.WriteLine("Минимальный элемент в массиве: " + min);
-double result = max - min;
-Console.WriteLine("Разница между max и min элементами = " + result);
+float result = max - min;
+Console.WriteLine("Разница между max и min элементами = " + Math.Round(result,2));
